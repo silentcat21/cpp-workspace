7921fbea54155dfadffe92ae9cb1242bc6a8a8bc
silentcat21 <silencecat2014@gmail.com>
2021-08-13 13:42:07 +0900

    chapter2 추가

---
+++ chapter01/ex02_cout.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+
+{
+    cout << 10;
+    cout << " apple" <<endl;
+
+    cout << endl;
+
+    cout << 10 << endl;
+    return 0;
+}
